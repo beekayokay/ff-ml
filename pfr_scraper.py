c8025f3e14bd6e60a73b8ef0e6b361083611e3e4
Brian Ko <beekayokay@Brians-iMac.local>
2020-06-27 17:12:35 -0700

    stable database scraber - basic

--- pfr_scraper.py
+++ pfr_scraper.py
@@ -1,48 +1,100 @@
+from datetime import datetime
+
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.types import Float, Integer, String
 
 
 class GameScraper:
     def __init__(self, url):
         self.url = url
 
-    def create_df(self):
-        res = requests.get(self.url)
-        soup = BeautifulSoup(res.content, 'lxml')
-        table = soup.find_all('table')[0]
-        df = pd.read_html(str(table))
-        df = df[0]
-        df_col0 = df.columns.get_level_values(0)
-        df_col1 = df.columns.get_level_values(1)
-        col_list = []
-        for idx, each in enumerate(df_col0):
-            if each.startswith('Unnamed'):
-                zero_lvl = ""
-            else:
-                zero_lvl = each + ' | '
-            if df_col1[idx].startswith('Unnamed'):
-                one_lvl = 'Index_' + str(idx)
-            else:
-                one_lvl = df_col1[idx]
-            col_list.append(zero_lvl + one_lvl)
-        df.columns = col_list
-        df = df[pd.to_numeric(df['Rk'], errors='coerce').notnull()]
+    def create_df(self, col, count):
+        page = requests.get(self.url)
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, "lxml")
+            table = soup.find('tbody')
+            rows = table.findAll('tr')
+        df = pd.DataFrame(columns=col)
+        count = 0
+        for idx, item in enumerate(rows):
+            data_set = rows[idx].findAll('td')
+            for idx, item in enumerate(data_set):
+                if idx == 0:
+                    df.loc[count, 'player_id'] = item['data-append-csv']
+                    df.loc[count, 'player'] = item.text
+                else:
+                    if item['data-stat'] in col:
+                        try:
+                            df.loc[count, item['data-stat']] = (
+                                (datetime.strptime(item.text, '%Y-%m-%d') -
+                                    datetime(1970, 1, 1)).total_seconds()
+                            )
+                        except ValueError:
+                            try:
+                                df.loc[count, item['data-stat']] = (
+                                    round(float(item.text), 4)
+                                )
+                            except ValueError:
+                                df.loc[count, item['data-stat']] = item.text
+            count += 1
+        df.log_id = (
+            df.player_id + '-' +
+            df.game_date.astype(int).astype(str) + '-' +
+            df.week_num.astype(int).astype(str)
+        )
         return df
 
 
 class DatabaseMaker:
-    def __init__(self, db):
-        self.db = db
+    def __init__(self, df):
+        self.df = df
 
     def create_db(self):
-        Base = declarative_base()
-
-        class GameLog(Base):
-            __tablename__ = 'gamelog'
-
-            id = Column('id', Integer, primary_key=True)
-            player_id = Column('player_id', String)
+        engine = create_engine('sqlite:///test.db', echo=True)
+        table_name = 'gamelogs'
+        self.df.to_sql(
+            table_name,
+            con=engine,
+            if_exists='replace',  # change to append later
+            index=True,
+            # chunksize=500,
+            dtype={
+                'log_id': String,
+                'player_id': String,
+                'player': String,
+                'pos': String,
+                'age': Float,
+                'game_date': Integer,
+                'league_id': String,
+                'team': String,
+                'game_location': String,
+                'opp': String,
+                'game_result': String,
+                'game_num': Integer,
+                'week_num': Integer,
+                'game_day_of_week': String,
+                'fantasy_points': Float,
+                'fantasy_points_ppr': Float,
+                'draftkings_points': Float,
+                'fanduel_points': Float,
+                'pass_cmp': Integer,
+                'pass_att': Integer,
+                'pass_yds': Integer,
+                'pass_td': Integer,
+                'pass_int': Integer,
+                'rush_att': Integer,
+                'rush_yds': Integer,
+                'rush_td': Integer,
+                'rec': Integer,
+                'rec_yds': Integer,
+                'rec_td': Integer,
+                'fumbles': Integer,
+                'fgm': Integer,
+                'fga': Integer,
+                'xpm': Integer,
+                'xpa': Integer
+            }
+        )
--- test_execute.py
+++ test_execute.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 
-from pfr_scraper import GameScraper
+from pfr_scraper import GameScraper, DatabaseMaker
 
 config_parser = ConfigParser()
 config_parser.read('config.ini')
@@ -16,12 +16,20 @@ url = 'https://www.pro-football-reference.com/play-index/pgl_finder.cgi?'\
     'qb_start_num_min=1&qb_start_num_max=400&game_num_min=0&game_num_max=99&'\
     'week_num_min=0&week_num_max=99&c5val=1.0&order_by=fantasy_points'\
     f'&offset={page_off}'
+col_list = [
+    'log_id', 'player_id', 'player', 'pos', 'age', 'game_date', 'league_id',
+    'team', 'game_location', 'opp', 'game_result', 'game_num', 'week_num',
+    'game_day_of_week', 'fantasy_points', 'fantasy_points_ppr',
+    'draftkings_points', 'fanduel_points', 'pass_cmp', 'pass_att', 'pass_yds',
+    'pass_td', 'pass_int', 'rush_att', 'rush_yds', 'rush_td', 'rec', 'rec_yds',
+    'rec_td', 'fumbles', 'fgm', 'fga', 'xpm', 'xpa'
+]
 
 # config_parser.set('SETTINGS', 'offset', '100')
 # with open('config.ini', 'w') as configfile:
 #     config_parser.write(configfile)
 
 game_scrape = GameScraper(url)
-df = game_scrape.create_df()
-print(url)
-print(df)
+df = game_scrape.create_df(col_list, 0)
+db_maker = DatabaseMaker(df)
+db_maker.create_db()
